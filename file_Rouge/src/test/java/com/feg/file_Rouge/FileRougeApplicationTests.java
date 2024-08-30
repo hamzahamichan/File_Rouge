a0c0971c0550269628f2dc8a2ea762d094d2636c
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-30 16:09:18 +0100

    create projet file rouge

---
+++ file_Rouge/src/test/java/com/feg/file_Rouge/FileRougeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feg.file_Rouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileRougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
