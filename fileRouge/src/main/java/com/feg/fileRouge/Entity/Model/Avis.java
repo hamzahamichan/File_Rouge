59774ce3a2e3101ee6ca356fac9dee698b54aabc
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-03 23:27:05 +0100

    Create Entity Avis

---
+++ fileRouge/src/main/java/com/feg/fileRouge/Entity/Model/Avis.java
@@ -0,0 +1,29 @@
+package com.feg.fileRouge.Entity.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+
+public class Avis {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAvis;
+    private String commentaire;
+    private int note;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", referencedColumnName = "idClient")
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(name = "salle_id", referencedColumnName = "idSalle")
+    private Salle salle;
+}
