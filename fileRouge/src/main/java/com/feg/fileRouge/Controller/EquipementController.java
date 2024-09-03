79ea94a6bc491e77baf54ca0e5889bb8033ca308
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-03 23:44:06 +0100

    controller Equipement

---
+++ fileRouge/src/main/java/com/feg/fileRouge/Controller/EquipementController.java
@@ -0,0 +1,38 @@
+package com.feg.fileRouge.Controller;
+
+import com.feg.fileRouge.Entity.Dto.EquipementDto;
+import com.feg.fileRouge.Entity.Model.Equipement;
+import com.feg.fileRouge.Services.equipement.EquipementServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/equipement")
+@RestController
+public class EquipementController {
+  @Autowired
+    private EquipementServiceImpl service;
+
+    @PostMapping(value = "/ajouter")
+    ResponseEntity<Equipement> save(@RequestBody EquipementDto equipementDto){
+        return this.service.save(equipementDto);
+    }
+     @GetMapping("/afficher")
+    EquipementDto getEquipementById(@RequestParam Long id){
+        return this.service.getEquipementById(id);
+    }
+    @GetMapping("/liste")
+    List<Equipement> getAllEquipements(){
+        return this.service.getAllEquipements();
+    }
+    @PutMapping("/modifier")
+    EquipementDto updateEquipement(@RequestBody EquipementDto equipementDto){
+        return this.service.updateEquipement(equipementDto);
+    }
+    @DeleteMapping("/supprimer")
+    void deleteEquipement(@RequestParam Long id){
+        this.service.deleteEquipement(id);
+    }
+}
