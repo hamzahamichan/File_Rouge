6dac320cd52731995a07ec2f80689bae0031b1e7
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-03 23:36:53 +0100

    Create Salles Dto

---
+++ fileRouge/src/main/java/com/feg/fileRouge/Entity/Dto/SallesDto.java
@@ -0,0 +1,15 @@
+package com.feg.fileRouge.Entity.Dto;
+
+import lombok.*;
+
+@Data @AllArgsConstructor
+@NoArgsConstructor @Builder
+
+public class SallesDto {
+    private Long idSalle;
+    private String nom;
+    private String description;
+    private int capacite;
+    private String emplacement;
+
+}
