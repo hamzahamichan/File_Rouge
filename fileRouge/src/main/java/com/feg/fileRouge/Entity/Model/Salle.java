08c4608c536be8407f23ed8a8821a2a002869264
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-03 23:23:33 +0100

    Create Entity Salle

---
+++ fileRouge/src/main/java/com/feg/fileRouge/Entity/Model/Salle.java
@@ -0,0 +1,35 @@
+package com.feg.fileRouge.Entity.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+
+@Entity @Data @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class Salle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idSalle;
+    private String nom;
+    private String description;
+    private int capacite;
+    private String emplacement;
+
+    @OneToMany(mappedBy = "salle")
+    private List<Equipement> equipements;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "tarif_id", referencedColumnName = "idTarif")
+    private Tarif tarif;
+
+    @OneToMany(mappedBy = "salle")
+    private List<Avis> avis;
+
+    @OneToMany(mappedBy = "salle")
+    private List<Reservation> reservations;
+}
