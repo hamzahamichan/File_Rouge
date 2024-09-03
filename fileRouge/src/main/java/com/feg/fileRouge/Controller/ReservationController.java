4c470bef29e92dd5bd2fc564365d3056661d8a3d
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-03 23:44:34 +0100

    controller Reservation

---
+++ fileRouge/src/main/java/com/feg/fileRouge/Controller/ReservationController.java
@@ -0,0 +1,32 @@
+package com.feg.fileRouge.Controller;
+
+import com.feg.fileRouge.Entity.Dto.ReservationDto;
+import com.feg.fileRouge.Entity.Model.Reservation;
+import com.feg.fileRouge.Services.Reservation.ReservationServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/reservation")
+public class ReservationController {
+
+    private final ReservationServiceImpl service;
+
+    public ReservationController(ReservationServiceImpl service) {
+        this.service = service;
+    }
+
+    @PostMapping("/faire")
+    public ResponseEntity<Reservation> effectue (@RequestBody ReservationDto reservationDto){
+        return  this.service.save(reservationDto);
+    }
+    @GetMapping("/afficher")
+    public ReservationDto afficherReservationId(@RequestParam Long id){
+        return this.service.getReservationById(id);
+    }
+
+    @DeleteMapping("/supprimer")
+    public void supprimerReservation(@RequestParam Long id){
+        this.service.deleteReservation(id);
+    }
+}
