3aa5e7c2a4f36b7ba0133b9722a9682ba4fd35db
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-03 23:46:13 +0100

    Create SalleMapper

---
+++ fileRouge/src/main/java/com/feg/fileRouge/mapper/SalleMapper.java
@@ -0,0 +1,12 @@
+package com.feg.fileRouge.mapper;
+
+import com.feg.fileRouge.Entity.Dto.SallesDto;
+import com.feg.fileRouge.Entity.Model.Salle;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface SalleMapper{
+
+    SallesDto toDto(Salle salle);
+    Salle toEntity(SallesDto dto);
+}
