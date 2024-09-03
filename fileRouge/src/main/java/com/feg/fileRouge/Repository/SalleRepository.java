1d05e2f7177deab7f84681eec7672a6a406b1eb1
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-03 23:38:33 +0100

    Create Repository Salle

---
+++ fileRouge/src/main/java/com/feg/fileRouge/Repository/SalleRepository.java
@@ -0,0 +1,7 @@
+package com.feg.fileRouge.Repository;
+
+import com.feg.fileRouge.Entity.Model.Salle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalleRepository extends JpaRepository<Salle,Long> {
+}
