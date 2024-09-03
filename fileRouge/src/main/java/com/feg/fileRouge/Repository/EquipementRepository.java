568bd2e4d6fbdf032afe6744b354b678c2f705d0
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-03 23:38:53 +0100

    Create Repository Equipement

---
+++ fileRouge/src/main/java/com/feg/fileRouge/Repository/EquipementRepository.java
@@ -0,0 +1,7 @@
+package com.feg.fileRouge.Repository;
+
+import com.feg.fileRouge.Entity.Model.Equipement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipementRepository extends JpaRepository<Equipement,Long> {
+}
