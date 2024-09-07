7212b3f3d91ed5d956177a12d53f6d40b5efbc45
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-07 20:30:31 +0100

    configuration des path et des autorisation

---
+++ fileRouge/src/main/java/com/feg/fileRouge/Config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.feg.fileRouge.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthentificationFilter jwtAuthFilter;
+
+
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Use the new authorizeHttpRequests with lambdas
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for stateless API
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/v1/auth/register").permitAll() // Public endpoints
+                        .anyRequest().authenticated() // Any other request must be authenticated
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Use stateless session management
+                )
+                .authenticationProvider(authenticationProvider) // Configure the authentication provider
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // Add JWT filter before authentication filter
+
+        return http.build();
+    }
+}
