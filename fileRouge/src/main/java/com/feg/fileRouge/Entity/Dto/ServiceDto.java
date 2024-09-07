cf0718fe6ea19a40d24a0d4018ecd4c4eaaff2dd
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-07 20:39:29 +0100

    creation de services dto

---
+++ fileRouge/src/main/java/com/feg/fileRouge/Entity/Dto/ServiceDto.java
@@ -0,0 +1,16 @@
+package com.feg.fileRouge.Entity.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class ServiceDto {
+    private Long idService;
+    private String nom;
+    private String description;
+}
