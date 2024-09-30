872c97ae2106a198e5d0d5a0bfb6772682bda334
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-30 17:25:37 +0100

    validation des données d'unscreption

--- fileRouge/src/main/java/com/feg/fileRouge/Auth/RegisterRequest.java
+++ fileRouge/src/main/java/com/feg/fileRouge/Auth/RegisterRequest.java
@@ -1,6 +1,9 @@
 package com.feg.fileRouge.Auth;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,8 +12,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor @Builder
 @NoArgsConstructor @Data
 public class RegisterRequest {
+    @NotEmpty(message = "Le nom ne peut pas être vide.")
     private String nom;
+
+    @NotEmpty(message = "Le prénom ne peut pas être vide.")
     private String prenom;
+
+    @NotEmpty(message = "L'email ne peut pas être vide.")
+    @Email(message = "Format d'email invalide.")
     private String email;
+
+    @NotEmpty(message = "Le mot de passe ne peut pas être vide.")
+    @Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères.")
     private String motDePasse;
 }
