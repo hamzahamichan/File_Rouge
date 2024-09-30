2f96a6f68f566fef8430366f983827b2c33803f3
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-30 17:20:33 +0100

    validation des données de création d'un compte

--- fileRouge/src/main/java/com/feg/fileRouge/Auth/AutheticateRequest.java
+++ fileRouge/src/main/java/com/feg/fileRouge/Auth/AutheticateRequest.java
@@ -1,5 +1,7 @@
 package com.feg.fileRouge.Auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,6 +10,11 @@ import lombok.NoArgsConstructor;
 @Data @AllArgsConstructor
 @NoArgsConstructor @Builder
 public class AutheticateRequest {
+
+    @NotEmpty(message = "L'email ne peut pas être vide.")
+    @Email(message = "Format d'email invalide.")
     private String email;
+
+    @NotEmpty(message = "Le mot de passe ne peut pas être vide.")
     private String motDePasse;
 }
