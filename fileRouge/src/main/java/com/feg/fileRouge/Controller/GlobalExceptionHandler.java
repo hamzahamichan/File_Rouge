d556ae6518a1111c6872abbfeb4792a525f32f90
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-30 17:11:31 +0100

    creaton d'un controller pour gérer les exception

---
+++ fileRouge/src/main/java/com/feg/fileRouge/Controller/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.feg.fileRouge.Controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .reduce((first, second) -> first + ", " + second)
+                .orElse("Erreur de validation");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
+}
