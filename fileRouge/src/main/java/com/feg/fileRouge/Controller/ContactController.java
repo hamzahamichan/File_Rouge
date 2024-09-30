d6101861c6709b74c0c00935cd71c5b510372cc8
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-30 17:26:30 +0100

    implémentation de méthode de service contact

--- fileRouge/src/main/java/com/feg/fileRouge/Controller/ContactController.java
+++ fileRouge/src/main/java/com/feg/fileRouge/Controller/ContactController.java
@@ -5,20 +5,21 @@ import com.feg.fileRouge.Entity.Model.Contact;
 import com.feg.fileRouge.Services.contact.ContactservicImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/cn")
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@ControllerAdvice
 public class ContactController {
 
     @Autowired
     private  ContactservicImpl contactservic;
 
     @PostMapping("/contacte")
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Contact> save(@RequestBody ContactDto contactDto){
      return  this.contactservic.contacter(contactDto);
     }
