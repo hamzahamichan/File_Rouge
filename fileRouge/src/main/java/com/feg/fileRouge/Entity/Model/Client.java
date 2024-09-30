04a301d513748737b1993a17f8308923f8d72746
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-30 17:14:34 +0100

    validation des attibuts

--- fileRouge/src/main/java/com/feg/fileRouge/Entity/Model/Client.java
+++ fileRouge/src/main/java/com/feg/fileRouge/Entity/Model/Client.java
@@ -2,6 +2,9 @@ package com.feg.fileRouge.Entity.Model;
 
 import com.feg.fileRouge.Enum.Role;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,9 +25,19 @@ public class Client implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idClient;
+
+    @NotEmpty(message = "Le nom ne peut pas être vide.")
     private String nom;
+
+    @NotEmpty(message = "Le prénom ne peut pas être vide.")
     private String prenom;
+
+    @NotEmpty(message = "L'email ne peut pas être vide.")
+    @Email(message = "Format d'email invalide.")
     private String email;
+
+    @NotEmpty(message = "Le mot de passe ne peut pas être vide.")
+    @Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères.")
     private String motDePasse;
 
     @Enumerated(EnumType.STRING)
@@ -44,31 +57,37 @@ public class Client implements UserDetails {
 
     @Override
     public String getPassword() {
+
         return motDePasse;
     }
 
     @Override
     public String getUsername() {
+
         return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
+
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
+
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
+
         return true;
     }
 
     @Override
     public boolean isEnabled() {
+
         return true;
     }
 }
